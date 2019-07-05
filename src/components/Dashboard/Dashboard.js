591e94ab4e372a7eaf16a0052a07df7d4d0636ca
Spence Robison <spence@MacBook-Pro.local>
2019-07-05 09:52:49 -0600

    Starting Server

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        Dashboard
+        Product
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
